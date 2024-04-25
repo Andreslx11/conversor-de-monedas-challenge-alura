559b04ec5b84aa7b5b679ed03f06ff1fce49bd0e
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-04-25 12:02:13 -0500

    Agregue excepciones en la class ConsultarMoneda.java ConnectException, SocketTimeoutException,IOException | InterruptedException y en la class Principal.java agregue la excepción RuntimeException, con el fin de si hay una falla poder darle información al usuario idónea para un mejor servicio ejemplo: como si es por una falla de internet o por un ingreso de una opción dato invalido

--- src/ConsultarMoneda.java
+++ src/ConsultarMoneda.java
@@ -1,6 +1,8 @@
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,7 +13,7 @@ public class ConsultarMoneda {
 
         URI direccion = URI.create("https://v6.exchangerate-api.com/v6/" +
                 "80f9c1de39a6244d0a7045fc/latest/" + moneda);
-
+        try {
         // se instancia el cliente que va ser requests
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -21,17 +23,25 @@ public class ConsultarMoneda {
 
         // con la libreria Gson se paso el response a un objeto  MonedaExchangeRateApi
         // para trabajar con esa estructura
-        try {
+
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
             return new Gson().fromJson(response.body(), MonedaExchangeRateApi.class);
 
 
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error ");
+        } catch (ConnectException e){
+            System.err.println("Verifique su conexion a intenert");
 
+        } catch(SocketTimeoutException e){
+            System.err.println("El servidor no responde tiempo de espera agotado");
 
         }
+        catch (IOException | InterruptedException e) {
+            System.err.println("Error por favor reinicie el programa ");
+
+        }
+        return null;
     }
 
 }
+
--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,17 @@
+
+
 public class Principal {
     public static void main(String[] args) {
         ConsultarMoneda moneda = new ConsultarMoneda();
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
         ConversorMoneda conversorMoneda = new ConversorMoneda();
 
+           try {
+               boolean continuar = true;
+               while (continuar) {
 
-        boolean continuar = true;
-        while (continuar) {
-
-            // Diespliega el menu
-            interfaceUsuario.menu();
+                   // Diespliega el menu
+                   interfaceUsuario.menu();
 
             /*
             retorna 2 valores tipo String que son codigos  ISO para indentificar
@@ -17,34 +19,41 @@ public class Principal {
             son guardados en menoria con indetificador monedaPar
              */
 
-            Pair<String, String> monedaPar = interfaceUsuario.opcionInput();
+                   Pair<String, String> monedaPar = interfaceUsuario.opcionInput();
 
 
-            String salida = monedaPar.getFirst();
-            if (salida == "salir") {
-                continuar = false;
-            } else {
-                // se extrae los valores del Pair monedaPAr
-                String monedaInicial = monedaPar.getFirst();
-                String monedaFinal = monedaPar.getSecond();
+                   String salida = monedaPar.getFirst();
+                   if (salida == "salir") {
+                       continuar = false;
+                   } else {
+                       // se extrae los valores del Pair monedaPAr
+                       String monedaInicial = monedaPar.getFirst();
+                       String monedaFinal = monedaPar.getSecond();
 
-                // hace la consulta moneda (interviene el proceso: record MonedaExchangeRateApi,
-                // class ConsultarMoneda ) consume la api
-                var monedaDeCambio = moneda.buscarMoneda(monedaInicial);
+                       // hace la consulta moneda (interviene el proceso: record MonedaExchangeRateApi,
+                       // class ConsultarMoneda ) consume la api
+                       var monedaDeCambio = moneda.buscarMoneda(monedaInicial);
 
-                // extrae lo valores de las monedas del json aninidado conversion_rates
-                // que de la api
-                Double monedaOrigen = monedaDeCambio.conversion_rates().get(monedaInicial);
-                Double monedaDestino = monedaDeCambio.conversion_rates().get(monedaFinal);
-
-
-                // hace la conversión e imprime por consola los resultado con un string informativo
-                //para el usuario
-                conversorMoneda.conversorMoneda(monedaDestino, monedaOrigen, monedaInicial, monedaFinal);
-            }
+                       // extrae lo valores de las monedas del json aninidado conversion_rates
+                       // que de la api
+                       Double monedaOrigen = monedaDeCambio.conversion_rates().get(monedaInicial);
+                       Double monedaDestino = monedaDeCambio.conversion_rates().get(monedaFinal);
 
 
-        }
+                       // hace la conversión e imprime por consola los resultado con un string informativo
+                       //para el usuario
+                       conversorMoneda.conversorMoneda(monedaDestino, monedaOrigen, monedaInicial, monedaFinal);
+                   }
+
+               }
+           }catch (RuntimeException  e){
+               System.out.println("Finalizando la aplicación.");
+           }
+
+
+
+
+
 
     }
 }
